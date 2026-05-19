499cef1051615302b8a6b2f37fc64c0bad6f078f
ezzzni <unin5412@gmail.com>
2026-05-19 22:10:10 +0900

    feat: input field 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import CompanyListPage from "./pages/CompanyListPage";
+import UiDesignTestPage from "./pages/UiDesignTestPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<CompanyListPage />} />
+      <Route path="/test" element={<UiDesignTestPage />} />
       <Route path="*" element={<div>404 - 페이지를 찾을 수 없습니다.</div>} />
     </Routes>
   );
