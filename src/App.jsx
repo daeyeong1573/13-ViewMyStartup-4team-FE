4faeb9432a216bdfe1a81843f65e213017276fa9
Eunjin <143705791+Eznnni@users.noreply.github.com>
2026-05-19 23:20:02 +0900

    feat: input field 컴포넌트 구현 (#3)
    * feat: input field 컴포넌트 구현
    
    * chore: NotFoundPage 생성
    
    * chore: .inactive background 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import CompanyListPage from "./pages/CompanyListPage";
+import UiDesignTestPage from "./pages/UiDesignTestPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<CompanyListPage />} />
-      <Route path="*" element={<div>404 - 페이지를 찾을 수 없습니다.</div>} />
+      <Route path="/test" element={<UiDesignTestPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
---
+++ src/pages/UiDesignTestPage.jsx
@@ -0,0 +1,9 @@
+function UiDesignTestPage() {
+  return (
+    <>
+      <p>공통 컴포넌트를 구현할 임시 페이지입니다.</p>
+    </>
+  );
+}
+
+export default UiDesignTestPage;
