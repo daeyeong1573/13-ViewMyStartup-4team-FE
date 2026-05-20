2a0c2d840109cd48f7ba7ee9d8da260b35a87e56
ezzzni <unin5412@gmail.com>
2026-05-20 18:16:48 +0900

    chore: 중복된 비활성화 로직  제거

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -10,7 +10,7 @@ function Button({ children, onClick, variant = "solid", status = "active" }) {
     <button
       type="button"
       className={buttonClass}
-      onClick={status !== "inactive" ? onClick : undefined}
+      onClick={onClick}
       disabled={status === "inactive"}
     >
       {variant === "reset" && (
