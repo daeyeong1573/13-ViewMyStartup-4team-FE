2277de7e4a1823cf96e7bd599b35d98bc40ccf88
Eunjin <143705791+Eznnni@users.noreply.github.com>
2026-05-21 15:35:46 +0900

    feat: 리스트, 인풋 아이템 구현 (#11)
    * feat: 리스트 아이템 구현
    
    * feat: input 컴포넌트 구현
    
    * chore: CompanyList 컴포넌트명 파일명에 맞게 CompanyListItem으로 수정

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import styles from "./Input.module.css";
+import EyeOn from "../../assets/icons/btn_visibility.png";
+import EyeOff from "../../assets/icons/btn_invisibility.png";
+
+function Input({
+  placeholder,
+  value,
+  status = "default",
+  type = "text",
+  onChange,
+  errorMessage,
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+
+  function togglePassword() {
+    setShowPassword((prev) => !prev);
+  }
+
+  const inputType = isPassword ? (showPassword ? "text" : "password") : type;
+  const statusClass = errorMessage ? styles.error : styles[status];
+  const wrapperClass = `${styles.wrapper} ${statusClass}`;
+
+  return (
+    <div className={styles.container}>
+      <div className={wrapperClass}>
+        <input
+          type={inputType}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={styles.input}
+          autoComplete={isPassword ? "current-password" : undefined}
+        />
+        {isPassword && (
+          <button
+            type="button"
+            onClick={togglePassword}
+            className={styles.eyeIcon}
+          >
+            <img src={showPassword ? EyeOn : EyeOff} alt="비밀번호 보기 토글" />
+          </button>
+        )}
+      </div>
+      {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+    </div>
+  );
+}
+
+export default Input;
