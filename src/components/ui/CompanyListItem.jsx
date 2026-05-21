790b7227f6206a40fb9ba7d9cd2502c3b4d030dc
ezzzni <unin5412@gmail.com>
2026-05-21 11:12:33 +0900

    feat: 리스트 아이템 구현

---
+++ src/components/ui/CompanyListItem.jsx
@@ -0,0 +1,39 @@
+import styles from "./CompanyListItem.module.css";
+import Button from "./Button.jsx";
+import defaultLogo from "../../assets/images/default_image.png";
+
+function CompanyList({
+  image,
+  name,
+  category,
+  variant,
+  status,
+  onButtonClick,
+  buttonText,
+}) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentBox}>
+        <div className={styles.imageWrapper}>
+          <img
+            src={image || defaultLogo}
+            alt={`기업 ${name} 로고`}
+            className={styles.logoImage}
+            onError={(e) => {
+              e.target.src = defaultLogo;
+            }}
+          />
+        </div>
+        <div className={styles.infoBox}>
+          <div className={styles.companyName}>{name}</div>
+          <div className={styles.categoryText}>{category}</div>
+        </div>
+      </div>
+      <Button variant={variant} status={status} onClick={onButtonClick}>
+        {buttonText}
+      </Button>
+    </div>
+  );
+}
+
+export default CompanyList;
