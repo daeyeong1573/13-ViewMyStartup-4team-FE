bacb500ccc463e1049b73f9f726103f0228550cc
ANNDAILY <ymabox@naver.com>
2026-05-26 15:46:32 +0900

    feat: 투자 현황 리스트 조회 API 함수 추가

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -33,3 +33,17 @@ export const API = {
     request(endpoint, { method: "PATCH", body: JSON.stringify(data) }),
   delete: (endpoint) => request(endpoint, { method: "DELETE" }),
 };
+//[GET] 투자 현황 리스트 조회 API
+export const getInvestmentStatusList = async ({
+  page = 1,
+  limit = 10,
+  orderBy = "virtualInvestment_desc",
+}) => {
+  const queryParams = new URLSearchParams({
+    page: String(page),
+    limit: String(limit),
+    orderBy,
+  }).toString();
+
+  return await API.get(`/investments?${queryParams}`);
+};
