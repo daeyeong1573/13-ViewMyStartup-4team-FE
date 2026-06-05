12685ca9c451564799600b6ee5fca5de2b8c7b40
daeyeong1573 <rlaeodud9983@naver.com>
2026-06-06 01:54:57 +0900

    refact : gnb 반응형

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import GNB from "./GNB";
+import styles from "./Layout.module.css";
 
 function Layout() {
   return (
     <>
       <GNB />
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
     </>
