c7548d011bad2ebebd73aa57b4a8697562ea8a81
김대영 <rlaeodud9983@naver.com>
2026-06-06 23:57:13 +0900

    feat : 배포환경 설정 (#41)
    * chore : .env ignore
    
    * fix : modul.css로 인한 배포환경에서 css 미적용 현상
    
    * chore : 기존 로컬호스트 => .env로 변경

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000"; //배포후에는 .env로 관리합니다.
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const STARTUPS_ENDPOINT = "/startups";
 export const INVESTMENTS_ENDPOINT = "/investments";
